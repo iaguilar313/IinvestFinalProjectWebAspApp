53c2a7607929752882d87b73bf6eda341ecd3957
iaguilar313 <iaguilar@umich.edu>
2017-10-31 13:12:48 -0400

    initial commit

---
+++ iinvest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iinvest.MvcApplication" Language="C#" %>
---
+++ iinvest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iinvest.Startup))]
+namespace iinvest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
